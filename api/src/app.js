4377bdcc240744d534d427933de68e80e381ff4f
LiamOtegui <liamotegui@gmail.com>
2024-07-03 01:58:59 -0300

    Controllers added

--- api/src/app.js
+++ api/src/app.js
@@ -1,10 +1,11 @@
 const express = require('express')
 const app = express()
-// const cors = require('cors')
+const cors = require('cors')
 const router = require('./routes/index')
 
 app.use(express.json())
-// app.use(cors())
+app.use(express.urlencoded({extended: false})) // Para que se pueda usar "Form-encode" en el body y no solo formato "JSON" para el controller "PUT" de la ruta de productRouter.
+app.use(cors())
 app.use(router)
 
 module.exports = app
