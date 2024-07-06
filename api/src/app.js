7c982cca2fa912e60b6e98e24160468728ffe3d3
LiamOtegui <liamotegui@gmail.com>
2024-07-06 17:56:34 -0300

    Error Middleware added

--- api/src/app.js
+++ api/src/app.js
@@ -2,6 +2,12 @@ const express = require('express')
 const app = express()
 const cors = require('cors')
 const router = require('./routes/index')
+const errorMiddleware = require('../middlewares/errorMiddleware')
+
+app.get('/', (req, res) => {
+    res.send('Hello Node Api')
+})
+app.use(errorMiddleware)
 
 app.use(express.json())
 app.use(express.urlencoded({ extended: false })) // Para que se pueda usar "Form-encode" en el body y no solo formato "JSON" para el controller "PUT" de la ruta de productRouter.
